a8f015f4212213c4a1d0478a7ef4d0b9a8ff70a4
TEdes-len <tervin@mail.bradley.edu>
2025-09-03 18:15:11 -0500

    Changes

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,11 +4,13 @@ public class EnemyController : MonoBehaviour
 {
     public float speed = 5.0f;
     private bool moveLeft = true;
+    private Rigidbody2D rb;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,6 +21,6 @@ public class EnemyController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2();
+        rb.linearVelocity = new Vector2(-speed, 0);
     }
 }
