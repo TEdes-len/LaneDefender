875a5a2f8a545a5529a9776e54e4f0c7723c4805
TEdes-len <tervin@mail.bradley.edu>
2025-09-03 11:15:28 -0500

    Changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,20 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
-    private Vector2 movement;
+    private Vector2 moveInput;
+    private Rigidbody2D rb; 
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = moveInput * speed;
     }
 }
