22c862abeee8b371ab83b0de2db6e2be6a7001fd
TEdes-len <tervin@mail.bradley.edu>
2025-09-03 12:04:30 -0500

    Changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
     private Vector2 moveInput;
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction fireAction;
+
+
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.currentActionMap.FindAction("Move");
+        moveAction.performed += MoveAction_performed;
+        moveAction.canceled += MoveAction_performed;
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void MoveAction_performed(InputAction.CallbackContext obj)
+    {
+       moveInput = moveAction.ReadValue<Vector2>();
+       
+    }
+
+    private void Shoot()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = moveInput * speed;
+        
+    }
+
+    private void FixedUpdate()
+    {
+       
+        rb.linearVelocity = new Vector2(moveInput.x * 0, moveInput.y * speed);
+
     }
 }
