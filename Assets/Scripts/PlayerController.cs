c28a0523d1e1786ad55a2f82d260b7db6ccbb175
TEdes-len <tervin@mail.bradley.edu>
2025-09-02 23:02:10 -0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    private Vector2 movement;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
